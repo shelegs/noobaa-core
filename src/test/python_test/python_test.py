9d1e15b429671eb8bd9065f0580d45c8ba2aad52
root <root@ip-172-31-94-8.ec2.internal>
2020-03-28 11:12:35 +0000

    modified:   .travis.yml 	modified:   src/test/python_test/python_test.py

--- src/test/python_test/python_test.py
+++ src/test/python_test/python_test.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3 
 import os
-from git import Repo
-currentDirectory = os.getcwd()
-r = Repo(currentDirectory)
-git = r.git
-repo_heads = r.heads # or it's alias: r.branches
-print (git.fetch('origin'))
-print (repo_heads)
+PULL_REQUEST_BRANCH='PULL_REQUEST_BRANCH'
+print (os.getenv(PULL_REQUEST_BRANCH))
 print ("Sheleg")
 
